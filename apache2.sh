c391b91eaa0fddbd6007168d46a01635d9378c7c
Nil Portugués <nilopc@gmail.com>
2013-10-01 01:24:58 +0200

    Update apache2.sh

--- apache2.sh
+++ apache2.sh
@@ -11,6 +11,69 @@ sudo a2enmod headers
 sudo a2enmod expires
 
 #############################################################################
+# BETTER PERFORMANT APACHE2
+#############################################################################
+
+echo '
+# ----------------------------------------------------------------------
+# UTF-8
+# ----------------------------------------------------------------------
+AddDefaultCharset utf-8
+AddCharset utf-8 .css .js
+
+# ----------------------------------------------------------------------
+# Gzip data
+# ----------------------------------------------------------------------
+AddOutputFilterByType DEFLATE text/plain text/html text/javascript text/css application/json$
+
+# ----------------------------------------------------------------------
+# Remove Image ETags
+# ----------------------------------------------------------------------
+<IfModule mod_headers.c>
+	Header unset ETag
+</IfModule>
+FileETag None
+
+# ----------------------------------------------------------------------
+# Expires Headers
+# ----------------------------------------------------------------------
+<IfModule mod_expires.c>
+	ExpiresActive on
+
+	# Perhaps better to whitelist expires rules? Perhaps.
+	  ExpiresDefault                          "access plus 1 month"
+
+	# cache.appcache needs re-requests in FF 3.6 (thanks Remy ~Introducing HTML5)
+	  ExpiresByType text/cache-manifest       "access plus 0 seconds"
+
+	# Your document html
+	  ExpiresByType text/html                 "access plus 0 seconds"
+
+	# Data
+	  ExpiresByType text/xml                  "access plus 0 seconds"
+	  ExpiresByType application/xml           "access plus 0 seconds"
+	  ExpiresByType application/json          "access plus 0 seconds"
+
+	# Feed
+	  ExpiresByType application/rss+xml       "access plus 1 hour"
+	  ExpiresByType application/atom+xml      "access plus 1 hour"
+
+	# Webfonts
+	  ExpiresByType application/x-font-ttf    "access plus 2 year"
+	  ExpiresByType font/opentype             "access plus 2 year"
+	  ExpiresByType application/x-font-woff   "access plus 2 year"
+	  ExpiresByType image/svg+xml             "access plus 2 year"
+	  ExpiresByType application/vnd.ms-fontobject "access plus 2 year"
+
+	<FilesMatch "\.(ico|txt|js|css|txt|jpe?g|png|gif|bmp|flv|mov|avi|mp4|mp3)$">
+		ExpiresActive on
+		ExpiresDefault "access plus 2 years"
+	</FilesMatch>
+</IfModule>
+' >> /etc/apache2/apache2.conf 
+
+
+#############################################################################
 # SECURING APACHE2
 #############################################################################
 echo "ServerSignature Off"  >> /etc/apache2/apache2.conf 
