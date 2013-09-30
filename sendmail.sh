c5990b181918de3571070209bf22994fd55d0790
Nil Portugués <nilopc@gmail.com>
2013-09-30 21:23:55 +0200

    Update sendmail.sh

--- sendmail.sh
+++ sendmail.sh
@@ -3,11 +3,12 @@
 sudo -i 
 sudo apt-get install -y build-essential libpcre3 libpcre3-dev
 
+## Installation
 sudo apt-get install -y opendkim opendkim-tools sendmail openssl 
 
 ## In order to not show the X-Authentication-Warning header
-echo "FEATURE('use_ct_file')" >> /etc/mail/submit.mc
-echo "define('_USE_CT_FILE_','1')dnl" >> /etc/mail/submit.mc
+echo "FEATURE('use_ct_file')"                             >> /etc/mail/submit.mc
+echo "define('_USE_CT_FILE_','1')dnl"                     >> /etc/mail/submit.mc
 echo "define('confCT_FILE','/etc/mail/trusted-users')dnl" >> /etc/mail/submit.mc
 echo 'www-data' >> nano /etc/mail/trusted-users
 
