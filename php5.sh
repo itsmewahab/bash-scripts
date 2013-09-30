0afa01e9158b7de45fea3394a3c405aaccd111c3
Nil Portugués <nilopc@gmail.com>
2013-09-30 20:00:01 +0200

    Create php5.sh

---
+++ php5.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+sudo -i
+
+#############################################################################
+# BASE PHP
+#############################################################################
+
+#############################################################################
+# PHP LIBRARIES
+#############################################################################
+
+
+#############################################################################
+# SENDMAIL
+#----------------------------------------------------------------------------
+# In order to not show the X-PHP-Originating-Script header
+#############################################################################
+
+if [ -f /etc/init.d/sendmail ];
+then
+  if [ -f /etc/php5/cgi/php.ini ];
+  then
+    echo "mail.add_x_header = Off" >> /etc/php5/cgi/php.ini 
+  fi
+  
+  if [ -f /etc/php5/cli/php.ini ];
+  then
+    echo "mail.add_x_header = Off" >> /etc/php5/cli/php.ini
+  fi
+  
+  if [ -f /etc/php5/apache2/php.ini ];
+  then
+    echo "mail.add_x_header = Off" >> /etc/php5/apache2/php.ini
+  fi    
+fi
+
+################################################
+## RESTART THE WEB SERVERS
+################################################
+
+if [ -f /etc/init.d/apache2 ];
+then
+  sudo service apache2 restart
+fi
+
+if [ -f /etc/init.d/apache2 ];
+then
+  sudo service nginx restart
+fi
