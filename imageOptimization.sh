415278d2a0f13e0dd2d6df50382ca347f0b4e2ce
Nil Portugués <nilopc@gmail.com>
2013-10-01 00:49:24 +0200

    Create imageOptimization.sh

---
+++ imageOptimization.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+## JPEG-OPTIM
+sudo apt-get install -y jpegoptim libjpeg-progs
+
+## GITSICLE
+sudo apt-get install -y gifsicle
+
+## OPTI-PNG && ADVPNG
+sudo apt-get install -y optipng advancecomp
