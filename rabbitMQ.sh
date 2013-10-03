79d509ed82946b7bee0b6651c1125fc6057552c9
Nil Portugués <nilopc@gmail.com>
2013-10-03 17:38:10 +0200

    Update rabbitMQ.sh

--- rabbitMQ.sh
+++ rabbitMQ.sh
@@ -5,12 +5,12 @@
 ########################################################################
 
 ## Add the repo for RabbitMQ
-echo 'deb http://www.rabbitmq.com/debian/ testing main' >> /etc/apt/sources.list
+sudo echo 'deb http://www.rabbitmq.com/debian/ testing main' >> /etc/apt/sources.list
 
 ## Add the Auth keys
-wget http://www.rabbitmq.com/rabbitmq-signing-key-public.asc
+sudo wget http://www.rabbitmq.com/rabbitmq-signing-key-public.asc
 sudo apt-key add rabbitmq-signing-key-public.asc
 
 ## Update and install
-apt-get update
+sudo apt-get update
 sudo apt-get install rabbitmq-server
