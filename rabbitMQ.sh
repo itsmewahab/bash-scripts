27bc112cf0e61e0be57bf86befc9f0a61ebde053
Nil Portugués <nilopc@gmail.com>
2013-10-04 10:09:28 +0200

    Update rabbitMQ.sh

--- rabbitMQ.sh
+++ rabbitMQ.sh
@@ -14,3 +14,12 @@ sudo apt-key add rabbitmq-signing-key-public.asc
 ## Update and install
 sudo apt-get update
 sudo apt-get install -y rabbitmq-server
+
+DATA="
+# I am a complete /etc/rabbitmq/rabbitmq-env.conf file.
+# Comment lines start with a hash character.
+# This is a /bin/sh script file - use ordinary envt var syntax
+NODENAME=rabbit_1
+"
+
+sudo echo $DATA >> /etc/rabbitmq/rabbitmq-env.conf 
