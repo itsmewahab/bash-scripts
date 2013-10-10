eaf91ba942e0c9f527111ca90350f913e96a4541
Nil Portugués <nilopc@gmail.com>
2013-10-10 22:50:36 +0200

    Update latestSoftware.sh

--- latestSoftware.sh
+++ latestSoftware.sh
@@ -1,4 +1,6 @@
-sudo apt-get install python-software-properties
+#!/bin/bash
+
+sudo apt-get install -y python-software-properties
 sudo add-apt-repository ppa:ondrej/php5
 sudo apt-get update
 sudo apt-get upgrade
