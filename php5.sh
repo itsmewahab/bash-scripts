9c9b67f4eeb1d11d233f31dff13638f5defcdec4
Nil Portugués <nilopc@gmail.com>
2013-10-11 13:17:27 +0200

    Update php5.sh

--- php5.sh
+++ php5.sh
@@ -9,7 +9,7 @@ sudo apt-get install -y build-essential libpcre3 libpcre3-dev
 #############################################################################
 # BASE PHP
 #############################################################################
-sudo apt-get install -y php5 php5-dev php5-cli php5-common php5-cli php5-cgi php-pear libgv-php5
+sudo apt-get install -y php5 php5-dev php5-cli php5-common php5-cli php5-cgi php-pear
 	
 #############################################################################
 # PHP LIBRARIES
@@ -160,7 +160,7 @@ then
   sudo service apache2 restart
 fi
 
-if [ -f /etc/init.d/apache2 ];
+if [ -f /etc/init.d/nginx ];
 then
   sudo service nginx restart
 fi
