54c5f87c95066a9a9e5c82fb81c7d1e65aa75792
Nil Portugués <nilopc@gmail.com>
2013-10-11 12:56:36 +0200

    Update memcached.sh

--- memcached.sh
+++ memcached.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
+sudo apt-get install -y python-software-properties
+echo "\n" | sudo -S add-apt-repository ppa:ondrej/php5
+sudo apt-get -y update
+sudo apt-get -y upgrade
 
 sudo apt-get -y install memcached libmemcache-dev libmemcached-dev zlib1g-dev libssl-dev
