36b9c010c30157570865a16778c2885a9fffae5a
Nil Portugués <nilopc@gmail.com>
2013-10-11 17:25:14 +0200

    Update and rename latestSoftware.sh to basics.sh

--- basics.sh
+++ basics.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-sudo apt-get install -y python-software-properties
+sudo apt-get install -y build-essential python-software-properties
 echo "\n" | sudo -S add-apt-repository ppa:ondrej/php5
+
 sudo apt-get -y update
 sudo apt-get -y upgrade
