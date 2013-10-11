e9eed89c94086daaa27f56aad36a02a640e6cec0
Nil Portugués <nilopc@gmail.com>
2013-10-11 13:07:27 +0200

    Update mySQL.sh

--- mySQL.sh
+++ mySQL.sh
@@ -4,5 +4,5 @@ echo "\n" | sudo -S add-apt-repository ppa:ondrej/php5
 sudo apt-get -y update
 sudo apt-get -y upgrade
 
-sudo apt-get -y install mysql-client mysql-server libapache2-mod-auth-mysql libsasl2-2 sasl2-bin libsasl2-2 libsasl2-dev libsasl2-modules
+sudo apt-get -y install mysql-client mysql-server libsasl2-2 sasl2-bin libsasl2-2 libsasl2-dev libsasl2-modules
 
