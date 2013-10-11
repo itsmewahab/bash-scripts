241221a708b7202d23f2fd58d80f447c67f5dafd
Nil Portugués <nilopc@gmail.com>
2013-10-11 12:56:25 +0200

    Update apache2.sh

--- apache2.sh
+++ apache2.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+sudo apt-get install -y python-software-properties
+echo "\n" | sudo -S add-apt-repository ppa:ondrej/php5
+sudo apt-get -y update
+sudo apt-get -y upgrade
 
 sudo apt-get install -y apache2 apache2-dev libapache2-mod-geoip
 
