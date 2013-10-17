b2165865babaa7549bc06cd6b3df059fcc0167b9
Nil Portugués <nilopc@gmail.com>
2013-10-17 02:11:22 +0200

    Update php5.sh

--- php5.sh
+++ php5.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
+
+if [ "$UID" -ne "0" ]
+then
+  echo "" 
+  echo "You must be sudoer or root. To run this script enter:"
+  echo ""
+  echo "sudo chmod +x $0; sudo ./$0"
+  echo ""
+  exit 1
+fi
+
 sudo apt-get install -y python-software-properties
 echo "\n" | sudo -S add-apt-repository ppa:ondrej/php5
 sudo apt-get -y update
@@ -128,3 +139,5 @@ if [ -f /etc/init.d/nginx ];
 then
   sudo service nginx restart
 fi
+
+exit 0
