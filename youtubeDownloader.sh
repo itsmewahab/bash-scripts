de8e57e79696cedfbe395731d4ec7e74d05dd0e9
Nil Portugués <nilopc@gmail.com>
2013-10-17 02:08:47 +0200

    Update youtubeDownloader.sh

--- youtubeDownloader.sh
+++ youtubeDownloader.sh
@@ -2,7 +2,11 @@
 
 if [ "$UID" -ne "0" ]
 then
-  echo; echo "You must be root to run this script. Try sudo ./$0"; echo
+  echo "" 
+  echo "You must be sudoer or root. To run this script enter:"
+  echo ""
+  echo "sudo chmod +x $0; sudo ./$0"
+  echo ""
   exit 1
 fi
 
