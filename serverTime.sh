392890a3837fc5833ba088b703296c7a87319f47
Nil Portugués <nilopc@gmail.com>
2013-10-17 02:12:29 +0200

    Update serverTime.sh

--- serverTime.sh
+++ serverTime.sh
@@ -1,5 +1,17 @@
-#!/bin/bash 
+#!/bin/bash
+
+if [ "$UID" -ne "0" ]
+then
+  echo "" 
+  echo "You must be sudoer or root. To run this script enter:"
+  echo ""
+  echo "sudo chmod +x $0; sudo ./$0"
+  echo ""
+  exit 1
+fi
 
 sudo ln -sf /usr/share/zoneinfo/Europe/Madrid /etc/localtime
 sudo apt-get install -y ntp
 sudo service ntp restart
+
+exit 0
