1d1fbe3cac34f280ec4e4e29871dcd2390583c7c
Nil Portugués <nilopc@gmail.com>
2013-10-17 02:11:08 +0200

    Update nodejs.sh

--- nodejs.sh
+++ nodejs.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 
+if [ "$UID" -ne "0" ]
+then
+  echo "" 
+  echo "You must be sudoer or root. To run this script enter:"
+  echo ""
+  echo "sudo chmod +x $0; sudo ./$0"
+  echo ""
+  exit 1
+fi
+
 echo "\n" | sudo add-apt-repository ppa:chris-lea/node.js  
 sudo apt-get update  
 sudo apt-get upgrade
 
 sudo apt-get install nodejs
+
+exit 0
