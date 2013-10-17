c83ab8024273f751466234705581d4be4583fc6f
Nil Portugués <nilopc@gmail.com>
2013-10-17 02:08:58 +0200

    Update amazonS3Cmd.sh

--- amazonS3Cmd.sh
+++ amazonS3Cmd.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
+if [ "$UID" -ne "0" ]
+then
+  echo "" 
+  echo "You must be sudoer or root. To run this script enter:"
+  echo ""
+  echo "sudo chmod +x $0; sudo ./$0"
+  echo ""
+  exit 1
+fi
+
 sudo apt-get install -y s3cmd
