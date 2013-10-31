4ecded797b45fa799dc6b797c4c43bebfb4843c9
Nil Portugués <nilopc@gmail.com>
2013-10-31 18:23:13 +0100

    Create aptFast.sh

---
+++ aptFast.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+apt-get install -y aria2
+
+## ADD THE REPO
+UBUNTU_NAME=`grep DISTRIB_CODENAME /etc/lsb-release  | awk -F "=" {'print $2'}`
+
+echo "deb http://ppa.launchpad.net/apt-fast/stable/ubuntu $UBUNTU_NAME main" 		>> /etc/apt/sources.list
+echo "deb-src http://ppa.launchpad.net/apt-fast/stable/ubuntu $UBUNTU_NAME main" 	>> /etc/apt/sources.list
+
+## INSTALL 
+apt-get upgrade
+apt-get update
+apt-get install -y apt-fast --force-yes
