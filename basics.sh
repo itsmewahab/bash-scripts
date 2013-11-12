f749ec7bd506420621878dff60c647e6f2a8d7cf
Nil Portugués <nilopc@gmail.com>
2013-11-12 13:17:17 +0100

    Update basics.sh

--- basics.sh
+++ basics.sh
@@ -9,13 +9,27 @@ then
   echo ""
   exit 1
 fi
+
 sudo apt-get -y update
 sudo apt-get -y upgrade
 
-sudo apt-get install -y git-core build-essential python-software-properties
+## Latest php5 version
+sudo apt-get install -y build-essential python-software-properties
 echo "\n" | sudo -S add-apt-repository ppa:ondrej/php5
-
 sudo apt-get -y update
 sudo apt-get -y upgrade
+
+## Git and git config
+sudo apt-get install -y git-core
+git config --system color.ui "auto"
+git config --system color.branch "auto"
+git config --system color.diff "auto"
+git config --system color.interactive "auto"
+git config --system color.status "auto"
+git config --system alias.st "status"
+git config --system alias.br "branch"
+git config --system alias.co "checkout"
+git config --system alias.df "diff"
+git config --system alias.lg "log -p"
 
 exit 0
