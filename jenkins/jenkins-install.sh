de1b07652d6ff1ee95e56dcc3e485db22396c0b7
IDODI Deploy <deploy@idodi.eu>
2013-12-27 13:54:07 +0100

    Disable Jenkins SSH StrictHostKeyChecking and KnownHostFile errors

--- jenkins/jenkins-install.sh
+++ jenkins/jenkins-install.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
-wget -q -O - http://pkg.jenkins-ci.org/debian/jenkins-ci.org.key | apt-key add -
-echo deb http://pkg.jenkins-ci.org/debian binary/ > /etc/apt/sources.list.d/jenkins.list
-apt-get update
-apt-get install jenkins
+## Download Jenkins
+sudo wget -q -O - http://pkg.jenkins-ci.org/debian/jenkins-ci.org.key | apt-key add -
+sudo echo deb http://pkg.jenkins-ci.org/debian binary/ > /etc/apt/sources.list.d/jenkins.list
+sudo apt-get update
+sudo apt-get install jenkins
+
+## Configure Jenkins SSH
+sudo su - jenkins
+cd
+ssh-keygen
+
+## Disable warning yes/no when doing ssh connections using git
+echo 'StrictHostKeyChecking no' >>  ~/.ssh/config
+echo 'UserKnownHostsFile /dev/null' >>  ~/.ssh/config
