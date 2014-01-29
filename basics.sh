43e2a9549a20db90f00110bb501a94d64e13c3b0
Nil Portugués Calderó <nilopc@users.noreply.github.com>
2014-01-29 19:17:25 +0100

    Update basics.sh

--- basics.sh
+++ basics.sh
@@ -13,6 +13,9 @@ fi
 sudo apt-get -y update
 sudo apt-get -y upgrade
 
+## Install Java. Always comes in handy
+sudo apt-get -y install default-jre
+
 ## Latest php5 version
 sudo apt-get install -y build-essential python-software-properties
 echo "\n" | sudo -S add-apt-repository ppa:ondrej/php5
