c5d7ed4c139a29c3820910183dd27d3fda64ae42
Nil Portugués Calderó <nilopc@users.noreply.github.com>
2014-02-19 11:45:39 +0100

    Update webp.sh

--- webp.sh
+++ webp.sh
@@ -1,4 +1,23 @@
-sudo -i
+#!/bin/bash
+
+############################################################################
+#
+# Author: Nil Portugués Calderó <contact@nilportugues.com>
+#
+# For the full copyright and license information, please view the LICENSE
+# file that was distributed with this source code.
+#
+############################################################################
+
+if [ "$UID" -ne "0" ]
+then
+  echo "" 
+  echo "You must be sudoer or root. To run this script enter:"
+  echo ""
+  echo "sudo chmod +x $0; sudo ./$0"
+  echo ""
+  exit 1
+fi
 
 cd
 wget https://webp.googlecode.com/files/libwebp-0.3.1-linux-x86-64.tar.gz
