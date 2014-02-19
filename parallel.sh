bb0c62f66f008ba0ed26cdbca0b2756aba66d85b
Nil Portugués Calderó <nilopc@users.noreply.github.com>
2014-02-19 11:39:38 +0100

    Update parallel.sh

--- parallel.sh
+++ parallel.sh
@@ -1,6 +1,14 @@
 #!/bin/bash 
 
-sudo -i
-wget http://launchpadlibrarian.net/104233941/parallel_20120422-1_all.deb
-dpkg -i parallel_20120422-1_all.deb
-rm parallel_20120422-1_all.deb
+############################################################################
+#
+# Author: Nil Portugués Calderó <contact@nilportugues.com>
+#
+# For the full copyright and license information, please view the LICENSE
+# file that was distributed with this source code.
+#
+############################################################################
+
+sudo wget http://launchpadlibrarian.net/104233941/parallel_20120422-1_all.deb
+sudo dpkg -i parallel_20120422-1_all.deb
+sudo rm parallel_20120422-1_all.deb
