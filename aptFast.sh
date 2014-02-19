7ac0ece6d1b86ec15a039410907af64b4e4ac62b
Nil Portugués Calderó <nilopc@users.noreply.github.com>
2014-02-19 11:35:44 +0100

    Update aptFast.sh

--- aptFast.sh
+++ aptFast.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+############################################################################
+#
+# Author: Nil Portugués Calderó <contact@nilportugues.com>
+#
+# For the full copyright and license information, please view the LICENSE
+# file that was distributed with this source code.
+#
+############################################################################
+ 
 apt-get install -y aria2
 
 ## ADD THE REPO
