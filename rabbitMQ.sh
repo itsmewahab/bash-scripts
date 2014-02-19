b0b08536fd97c2b26efe0d86cb37ba4cf5fb115c
Nil Portugués Calderó <nilopc@users.noreply.github.com>
2014-02-19 11:40:13 +0100

    Update rabbitMQ.sh

--- rabbitMQ.sh
+++ rabbitMQ.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+############################################################################
+#
+# Author: Nil Portugués Calderó <contact@nilportugues.com>
+#
+# For the full copyright and license information, please view the LICENSE
+# file that was distributed with this source code.
+#
+############################################################################
+
 if [ "$UID" -ne "0" ]
 then
   echo "" 
