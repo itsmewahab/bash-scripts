50bd6d0b19c626460a22c30f1501dc0ad26be2c2
Nil Portugués Calderó <nilopc@users.noreply.github.com>
2014-02-19 11:45:06 +0100

    Update w3c_validator.sh

--- w3c_validator.sh
+++ w3c_validator.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
+############################################################################
+#
+# Author: Nil Portugués Calderó <contact@nilportugues.com>
+#
+# For the full copyright and license information, please view the LICENSE
+# file that was distributed with this source code.
+#
+############################################################################
+
 ## http://blog.simplytestable.com/installing-the-w3c-html-validator-with-html5-support-on-ubuntu/
-sudo -i 
+
 sudo apt-get -y install w3c-markup-validator
-sed -i 's/Allow Private IPs = no/Allow Private IPs = yes/g' /etc/w3c/validator.conf
+sudo sed -i 's/Allow Private IPs = no/Allow Private IPs = yes/g' /etc/w3c/validator.conf
