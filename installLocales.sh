a1086562a5dcd613950e1b9d19d77984fd088301
Nil Portugués Calderó <nilopc@users.noreply.github.com>
2014-02-19 11:36:20 +0100

    Update installLocales.sh

--- installLocales.sh
+++ installLocales.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
+############################################################################
+#
+# Author: Nil Portugués Calderó <contact@nilportugues.com>
+#
+# For the full copyright and license information, please view the LICENSE
+# file that was distributed with this source code.
+#
+############################################################################
+ 
 sudo cp /usr/share/i18n/SUPPORTED /var/lib/locales/supported.d/local
 sudo locale-gen
