4c1379307607ad763633126fb6621acc451b7793
Nil Portugués Calderó <nilopc@users.noreply.github.com>
2014-02-24 14:03:40 +0100

    Create varnish.sh

---
+++ varnish.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+############################################################################
+#
+# Author: Nil Portugués Calderó <contact@nilportugues.com>
+#
+# For the full copyright and license information, please view the LICENSE
+# file that was distributed with this source code.
+#
+############################################################################
+
+if [ "$UID" -ne "0" ]
+then
+  echo "" 
+  echo "You must be sudoer or root. To run this script enter:"
+  echo ""
+  echo "sudo chmod +x $0; sudo ./$0"
+  echo ""
+  exit 1
+fi
+
+## INSTALL 
+if [ -z $(which varnish) ];
+then
+  sudo apt-get update
+  sudo apt-get install -y varnish
+fi
