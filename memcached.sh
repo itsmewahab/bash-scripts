f3039bf0e325619d94604b0a34ab0401ae294a70
Nil Portugués Calderó <nilopc@users.noreply.github.com>
2014-03-11 18:00:53 +0100

    Update memcached.sh

--- memcached.sh
+++ memcached.sh
@@ -25,3 +25,33 @@ sudo apt-get -y update
 sudo apt-get -y upgrade
 
 sudo apt-get -y install memcached libmemcache-dev libmemcached-dev zlib1g-dev libssl-dev
+
+##--------------------------------------------------------------------------
+## CONFIGURE THE MEMORY USED BY MEMCACHED
+##--------------------------------------------------------------------------
+echo '' >  /etc/default/memcached 
+echo 'ENABLE_MEMCACHED=yes' >> /etc/default/memcached 
+echo 'PORT="11211"' >> /etc/default/memcached 
+echo 'USER="memcached"' >> /etc/default/memcached 
+echo 'MAXCONN="1024"' >> /etc/default/memcached 
+echo 'CACHESIZE="512"' >> /etc/default/memcached 
+echo '## make sure we accept connection from external machines on our IP, eg: 192.168.1.12:11211' >> /etc/default/memcached 
+echo '##OPTIONS="-l 192.168.1.12 -L"' >> /etc/default/memcached 
+
+##--------------------------------------------------------------------------
+## CONFIGURE THE MAXIMUM PORTS A LOCAL SERVICE CAN USE.
+##--------------------------------------------------------------------------
+if [ $(cat /etc/sysctl.conf | grep 'fs.file-max = 50000' | wc -l) = "0" ];
+then
+  echo '# Increase system file descriptor limit to' >> /etc/sysctl.conf
+  echo 'fs.file-max = 50000' >> /etc/sysctl.conf
+fi
+
+
+if [ $(cat /etc/sysctl.conf | grep 'net.ipv4.ip_local_port_range = 2000 65000' | wc -l) = "0" ];
+then
+  echo '# Increase system IP port limits' >> /etc/sysctl.conf
+  echo 'net.ipv4.ip_local_port_range = 2000 65000' >> /etc/sysctl.conf
+fi
+
+sudo sysctl -p
