de3cd3edbc74745c200316b372926706460fe255
Nil Portugués Calderó <nilopc@users.noreply.github.com>
2014-04-07 23:40:23 +0200

    Update nginx.sh

--- nginx.sh
+++ nginx.sh
@@ -467,10 +467,11 @@ server {
   	## PHP: Pass all PHP request to PHP-FPM
 	location ~ \.php$ 
 	{
-		fastcgi_split_path_info ^(.+\.php)(/.+)$;
-		fastcgi_pass unix:/var/run/php5-fpm.sock;
-		fastcgi_index index.php;
-		include fastcgi_params;
+                fastcgi_split_path_info ^(.+\.php)(/.+)$;
+                fastcgi_pass unix:/var/run/php5-fpm.sock;
+                fastcgi_param SCRIPT_FILENAME $document_root$fastcgi_script_name;
+                fastcgi_intercept_errors on;
+                include fastcgi_params;
 	}
 }
 
