3e6f817a4664080fb62adbad7ba1b3b5aa3d6996
Nil Portugués Calderó <nilopc@gmail.com>
2014-07-21 16:48:04 +0200

    Create ionic-framework.sh

---
+++ ionic-framework.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+############################################################################
+#
+# Author: Nil Portugués Calderó <contact@nilportugues.com>
+#
+# For the full copyright and license information, please view the LICENSE
+# file that was distributed with this source code.
+#
+############################################################################
+
+if [ "$UID" -ne "0" ]
+then
+  echo "" 
+  echo "You must be sudoer or root. To run this script enter:"
+  echo ""
+  echo "sudo chmod +x $0; sudo ./$0"
+  echo ""
+  exit 1
+fi
+
+#############################################################
+## INSTALL NODEJS
+#############################################################
+echo "\n" | sudo add-apt-repository ppa:chris-lea/node.js  
+sudo apt-get update
+sudo apt-get install -y nodejs npm
+
+#############################################################
+# INSTALL APACHE CORDOVA
+#############################################################
+sudo npm install -g cordova
+
+#############################################################
+# INSTALL IONIC FRAMEWORK
+#############################################################
+sudo npm install -g ionic
