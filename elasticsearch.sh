9933aeb6a123884e3a932c9bddb8651fb5a99603
Nil Portugués Calderó <nilopc@gmail.com>
2014-12-15 16:38:36 +0100

    Update elasticsearch.sh

--- elasticsearch.sh
+++ elasticsearch.sh
@@ -9,6 +9,17 @@
 #
 ############################################################################
  
+if [ "$UID" -ne "0" ]
+then
+  echo "" 
+  echo "You must be sudoer or root. To run this script enter:"
+  echo ""
+  echo "sudo chmod +x $0; sudo ./$0"
+  echo ""
+  exit 1
+fi
+
+
 ## Check for java dependencies first and install if missing
 if [ -z $(which java) ];
 then
