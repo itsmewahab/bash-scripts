c0e9df4c307a601e8268391d0a3b2783002d364a
Nil Portugués Calderó <nilopc@gmail.com>
2015-01-24 10:58:58 +0100

    Update glasshfish4.sh

--- glasshfish4.sh
+++ glasshfish4.sh
@@ -4,9 +4,9 @@ sudo -i apt-get update
 sudo -i apt-get install oracle-java7-installer unzip
 
 wget download.java.net/glassfish/4.0/release/glassfish-4.0.zip
-unzip glassfish-4.0.zip -d /opt
+sudo unzip glassfish-4.0.zip -d /opt
+sudo chmod -Rf 777 /opt/glassfish4/glassfish/domains/
 echo 'export PATH=/opt/glassfish4/bin:$PATH' >> ~/.profile
 echo 'alias asadmin='/opt/glassfish4/bin/asadmin' >> ~/.bash_aliases
-
 
 echo 'Start webserver using: asadmin start-domain'
