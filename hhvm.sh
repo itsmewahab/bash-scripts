676cd866210fbeac84661cefc49b89d37c1a878e
Nil Portugués Calderó <nilopc@gmail.com>
2015-07-03 16:34:05 +0200

    Create hhvm.sh

---
+++ hhvm.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+############################################################################
+#
+# Author: Nil Portugués Calderó <contact@nilportugues.com>
+#
+# For the full copyright and license information, please view the LICENSE
+# file that was distributed with this source code.
+#
+############################################################################
+ 
+if [ "$UID" -ne "0" ]
+then
+  echo "" 
+  echo "You must be sudoer or root. To run this script enter:"
+  echo ""
+  echo "sudo chmod +x $0; sudo ./$0"
+  echo ""
+  exit 1
+fi
+
+# installs add-apt-repository
+sudo apt-get install software-properties-common
+
+sudo apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0x5a16e7281be7a449
+sudo add-apt-repository "deb http://dl.hhvm.com/ubuntu $(lsb_release -sc) main"
+sudo apt-get update
+sudo apt-get install hhvm
